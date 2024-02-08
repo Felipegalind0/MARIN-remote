722324e4849bd5f6fefd118d89b87c82e2a9ba51
Felipe Galindo <44674864+Felipegalind0@users.noreply.github.com>
2024-02-07 20:27:12 -0600

    add creds creds_template.h

---
+++ src/creds_template.h
@@ -0,0 +1,4 @@
+const char* ssid = "-"; // Your WiFi SSID
+const char* password = "-"; // Your WiFi Password
+const char* AP_ssid = "MARIN4"; // Your WiFi SSID
+const char* AP_password = "MARIN1010"; // Your WiFi Password
